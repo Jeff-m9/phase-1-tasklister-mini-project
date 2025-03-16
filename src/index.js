7442587c4067db86b461515549e50b1c4a70fecb
Jeff Mbithi <jeffmbithi9@gmail.com>
2025-03-16 17:30:44 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const form = document.getElementById("create-task-form");
+  
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    createTodo(event.target.elements["new-task-description"].value);
+    form.reset();
+
+  });
 });
+
+const createTodo = (todo) => {
+  let li = document.createElement("li");
+  let button = document.createElement("button");
+  
+
+  button.addEventListener("click", deleteTask);
+  button.textContent = "Done";
+
+  li.textContent = `${todo} `;
+  li.appendChild(button);
+
+  document.querySelector("#tasks").appendChild(li);
+};
+
+const deleteTask = (event) => {
+  event.target.parentNode.remove();
+};
+
+const dateDue = (e) => {
+  e.target.elements["date-due"].value;
+};
